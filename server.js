b522b6c52ecd2b2ad456ccf126732b037bae1224
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-04 15:52:57 +0900

    add: edit heroku config

--- server.js
+++ server.js
@@ -36,3 +36,10 @@ process.on('unhandledRejection', err => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECIEVED. Shutting down gracefully🙏');
+  server.close(() => {
+    console.log('process terminated❗');
+  });
+});
