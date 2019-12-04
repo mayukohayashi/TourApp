4ead1ef01c6dda55c5a7b7da16daafa41e09f993
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-04 16:20:59 +0900

    add: CORS in app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,17 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARE
+//Implement CORS
+app.use(cors());
+//Access-control-allow-origin *
+// app.use(
+//   cors({
+//     origin: 'https://workshopping.herokuapp.com/'
+//   })
+// );
+
+app.options('*', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
